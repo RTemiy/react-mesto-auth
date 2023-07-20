268827b448b98d769dba67883f6d57d39d09ceee
RTemiy <RTemiyGo@yandex.ru>
2023-07-20 20:11:48 +0300

    пр 12

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -3,5 +3,5 @@ import {Navigate} from "react-router-dom";
 export default function ProtectedRoute({loggedIn, element : Component, ...props}){
   return (
     loggedIn ? <Component {...props} /> : <Navigate to="/sign-in" replace/>
-  )
+  );
 }
